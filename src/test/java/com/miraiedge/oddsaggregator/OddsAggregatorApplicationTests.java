10904eed2167f0c98a0043e9d96128d311d54100
BhanuD19 <vaibhav.dounde1996@hotmail.com>
2025-06-14 07:15:10 +0530

    main: Create a skeleton project.

---
+++ src/test/java/com/miraiedge/oddsaggregator/OddsAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miraiedge.oddsaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OddsAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
